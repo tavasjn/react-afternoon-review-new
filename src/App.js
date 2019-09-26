332a7c3cfe62d6c0770049a7534f96be7a972ee9
Lucas <lucas@Ashleys-MacBook-Pro.local>
2019-09-26 16:48:16 -0600

    Initial App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <MainContent />
-    </div>
+    <>
+     Hello World!
+    </>
   );
 }
 
